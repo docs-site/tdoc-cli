c1ade9ac5ca6cc502045b5c5c3a168576fe2f9b0
苏木 <sumu@example.com>
2025-06-17 15:41:35 +0800

    创建index.ts并通过tsx运行器运行.

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/** =====================================================
+ * Copyright © hk. 2022-2025. All rights reserved.
+ * File name  : index.ts
+ * Author     : 苏木
+ * Date       : 2025-06-17
+ * Version    : 
+ * Description: 
+ * ======================================================
+ */
+
+console.log("Hello, World!");
