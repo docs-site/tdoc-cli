b5926dd9f8157d4b666c4f04a4e78cf3e3fbea66
苏木 <sumu@example.com>
2025-06-17 17:02:11 +0800

    创建tdoc命令

---
+++ bin/tdoc-cli.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+/** =====================================================
+ * Copyright © hk. 2022-2025. All rights reserved.
+ * File name  : tdoc-cli.js
+ * Author     : 苏木
+ * Date       : 2025-06-17
+ * Version    :
+ * Description: 导入index.js文件, 主要是为了添加第一行的 #!/usr/bin/env node
+ *              这一行用于指定脚本文件的解释程序。
+ * ======================================================
+ */
+
+import('../dist/index.js');
