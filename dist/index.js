ec2103248b208bda4d249b32589f9cb8bb1cc864
苏木 <sumu@example.com>
2025-06-17 19:54:48 +0800

    支持tdoc -v显示版本号

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+"use strict";
 /** =====================================================
  * Copyright © hk. 2022-2025. All rights reserved.
  * File name  : index.ts
@@ -7,4 +8,41 @@
  * Description:
  * ======================================================
  */
-console.log("Hello, World!");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var commander_1 = require("commander");
+var package_json_1 = __importDefault(require("../package.json"));
+/**
+ * @brief 创建commander的Command实例
+ */
+var program = new commander_1.Command(package_json_1.default.name);
+/**
+ * @brief 从package.json中提取项目版本和开发依赖信息
+ * @returns {string} 格式化的版本信息字符串，包含:
+ *                  - 项目名称和版本
+ *                  - 所有开发依赖项及其版本(每行一个依赖)
+ * @details 函数处理流程:
+ *          1. 使用Object.entries()获取devDependencies的键值对数组
+ *          2. 使用map()将每个依赖项格式化为"name: version"字符串
+ *          3. 使用join('\n')将所有依赖项合并为多行字符串
+ *          4. 返回包含项目名称、版本和格式化依赖信息的完整字符串
+ * @example 返回格式示例:
+ *          my-project: 1.0.0
+ *
+ *          devDependencies:
+ *            typescript: ^4.0.0
+ *            eslint: ^7.0.0
+ */
+function getVersionInfo() {
+    var depsInfo = Object.entries(package_json_1.default.devDependencies)
+        .map(function (_a) {
+        var name = _a[0], version = _a[1];
+        return "  ".concat(name, ": ").concat(version);
+    })
+        .join('\n');
+    return "".concat(package_json_1.default.name, ": ").concat(package_json_1.default.version, "\n\ndevDependencies:\n").concat(depsInfo);
+}
+program.version(getVersionInfo(), '-v, --version', '显示版本信息和依赖包');
+program.parse(); // 参数处理
